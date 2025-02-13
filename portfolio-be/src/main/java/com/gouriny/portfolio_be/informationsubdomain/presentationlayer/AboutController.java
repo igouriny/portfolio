3c41962065a4af6043d8a04ee460b95da87a1bc9
Ilyass Gouriny <2131685@champlaincollege.qc.ca>
2025-02-13 02:52:23 -0500

    Maybe this time?

--- portfolio-be/src/main/java/com/gouriny/portfolio_be/informationsubdomain/presentationlayer/AboutController.java
+++ portfolio-be/src/main/java/com/gouriny/portfolio_be/informationsubdomain/presentationlayer/AboutController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "https://portfolio-git-main-igourinys-projects.vercel.app")
 @RequestMapping("/api/v1/about")
 @RequiredArgsConstructor
 public class AboutController {
