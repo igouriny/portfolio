665dfeff2e013818540130e32e4be1f63ba3bbcf
Ilyass Gouriny <2131685@champlaincollege.qc.ca>
2025-02-13 01:53:17 -0500

    Deployment testing be/fe

--- portfolio-be/src/main/java/com/gouriny/portfolio_be/authservice/system/CorsConfig.java
+++ portfolio-be/src/main/java/com/gouriny/portfolio_be/authservice/system/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+//                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://portfolio-delta-nine-15.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
--- portfolio-fe/src/api/axiosInstance.js
+++ portfolio-fe/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://portfolio-production-ca67.up.railway.app/api/v1",
+  // baseURL: "http://localhost:8080/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
