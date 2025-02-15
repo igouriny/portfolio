35782242881bb3329eed2767240c934aa4b526e5
Ilyass Gouriny <2131685@champlaincollege.qc.ca>
2025-02-15 18:41:38 -0500

    Modified CORS again

--- portfolio-be/src/main/java/com/gouriny/portfolio_be/authservice/system/SecurityConfig.java
+++ portfolio-be/src/main/java/com/gouriny/portfolio_be/authservice/system/SecurityConfig.java
@@ -100,7 +100,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000",
-                "https://portfolio-git-main-igourinys-projects.vercel.app",
+                "https://portfolio-delta-nine-15.vercel.app",
                 "https://portfolio-igourinys-projects.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
