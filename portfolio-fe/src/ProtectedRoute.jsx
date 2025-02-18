3daac2454e0bb731afefb3bc34d195bd6a9ae882
Ilyass Gouriny <2131685@champlaincollege.qc.ca>
2025-02-17 23:17:33 -0500

    Fix

--- portfolio-fe/src/ProtectedRoute.jsx
+++ portfolio-fe/src/ProtectedRoute.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ user, children }) => {
-  // if (!user) {
-  //   return <Navigate to="/login" replace />;
-  // }
-  // return children;
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
 };
 
 export default ProtectedRoute;
